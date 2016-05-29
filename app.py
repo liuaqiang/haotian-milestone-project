0f2c7a4552fd1a4042a99d3eafd5fb0b2f2709b4
Haotian_Daniel_Liu <liuaqiang@gmail.com>
2016-05-29 14:15:15 -0400

    removing if arguments

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def main():
   return redirect('/index')
 
-@app.route('/index', methods = ['get', 'post'])
+@app.route('/index')#, methods = ['get', 'post'])
 def index():
     # nquestions = 5
     #return render_template('index.html')
-    if request.method == 'post':
-        app.vars['ticker'] = request.form.get('ticker')
-        app.vars['features'] = request.form.getlist('features')
+    # if request.method == 'post':
+    app.vars['ticker'] = request.form.get('ticker')
+    app.vars['features'] = request.form.getlist('features')
     #    return redirect('http://google.com')
     #    # app.vars['ticker'] = request.form['ticker']
     #    return redirect('https://www.quandl.com/api/v1/datasets/WIKI/' + 'GOOG' + '.json')
