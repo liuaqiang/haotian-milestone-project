f72ad83bf78c609f3d3395dc8bf2f817f4ff790b
Haotian_Daniel_Liu <liuaqiang@gmail.com>
2016-05-29 14:52:08 -0400

    Still trying to get data, changed the case of methods... I am so exhausted

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def index():
     # nquestions = 5
     # return render_template('index.html')
     # if request.method == 'post':
-    app.vars['ticker'] = request.form['ticker']
-    app.vars['features'] = request.form.getlist('features')
+    ticker  = request.form['ticker']
+    features = request.form.getlist('features')
     return redirect('http://google.com')
     #    # app.vars['ticker'] = request.form['ticker']
     #    return redirect('https://www.quandl.com/api/v1/datasets/WIKI/' + 'GOOG' + '.json')
