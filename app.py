c5e5f4a669fbf26b6c664d9c3e6e95f42b06bc4a
Haotian_Daniel_Liu <liuaqiang@gmail.com>
2016-05-29 13:17:21 -0400

    first try of plotting

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+import requests
+from bokeh.plotting import figure
+from bokeh.embed import components
 
 app = Flask(__name__)
 
@@ -12,3 +15,20 @@ def index():
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0')
+
+ticker = 'GOOG'
+
+# obtaining data through API
+api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/' + ticker + '.json'
+session = requests.Session()
+session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+raw_data = session.get(api_url)
+
+
+# Plotting
+plot = figure(tools=TOOLS,
+              title='Data from Quandle WIKI set',
+              x_axis_label='date',
+              x_axis_type='datetime')
+script, div = components(plot)
+return render_template('graph.html', script=script, div=div)
