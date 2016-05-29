33d3bfebd2cddf632f598c7e2c46afc5647736c1
Haotian_Daniel_Liu <liuaqiang@gmail.com>
2016-05-29 15:11:17 -0400

    just redirecting under method

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ def index():
     # nquestions = 5
     # return render_template('index.html')
     if request.method == 'post':
-    ticker  = request.form.get('ticker')
-    features = request.form.getlist('features')
-    # return redirect('http://google.com')
+    # ticker  = request.form.get('ticker')
+    # features = request.form.getlist('features')
+        return redirect('http://google.com')
     #    # app.vars['ticker'] = request.form['ticker']
     #    return redirect('https://www.quandl.com/api/v1/datasets/WIKI/' + 'GOOG' + '.json')
-    #else
-    #    return render_template('index.html',num=nquestions)
+    else
+        return render_template('index.html')
 
 # ticker = 'GOOG'
 
