32890d58fa4c56411b14f29d91c883458d2b5658
Haotian_Daniel_Liu <liuaqiang@gmail.com>
2016-05-29 13:40:31 -0400

    trying to redirect to data page

--- app.py
+++ app.py
@@ -11,24 +11,33 @@ def main():
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    nquestions = 5
+    if request.method == 'POST':
+        app.vars['ticker'] = request.form['ticker']
+        return redirect('https://www.quandl.com/api/v1/datasets/WIKI/' + app.vars['ticker'] + '.json')
+    else
+        return render_template('index.html',num=nquestions)
+
+# ticker = 'GOOG'
+
+# @app.route('/plot-'+ ticker,methods=['POST'])
+# def next_app():  #remember the function name does not need to match the URL
+#     return redirect('graph.html',)
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0')
 
-ticker = 'GOOG'
-
 # obtaining data through API
-api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/' + ticker + '.json'
-session = requests.Session()
-session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-raw_data = session.get(api_url)
+# api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/' + ticker + '.json'
+# session = requests.Session()
+# session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+# raw_data = session.get(api_url)
 
 
 # Plotting
-plot = figure(tools=TOOLS,
-              title='Data from Quandle WIKI set',
-              x_axis_label='date',
-              x_axis_type='datetime')
-script, div = components(plot)
-return render_template('graph.html', script=script, div=div)
+# plot = figure(tools=TOOLS,
+#               title='Data from Quandle WIKI set',
+#               x_axis_label='date',
+#               x_axis_type='datetime')
+# script, div = components(plot)
+# return render_template('graph.html', script=script, div=div)
