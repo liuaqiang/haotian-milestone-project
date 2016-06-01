846689c3fd4ae1c3785abb65cf4bbb167f640891
Haotian_Daniel_Liu <liuaqiang@gmail.com>
2016-06-01 13:46:50 -0400

    first attempt to plot

--- app.py
+++ app.py
@@ -27,13 +27,16 @@ def index():
         #return redirect('/graph')
     #else:
         #return render_template('index.html')
-
+#ticker = 'GOOG'
 @app.route('/graph')
 def graph():
+    page_title = "Stock Data for GOOG"
+    graph_title = "Generated Graph for GOOG"
+    
     x = np.random.random(10)
     y = np.random.random(10)
     
-    plot = figure(title="Stock Data for GOOG")
+    plot = figure(title = page_title)
     plot.line(x, y, line_width=2)
     
     #js_resources = INLINE.render_js()
@@ -41,7 +44,7 @@ def graph():
     
     script, div = components(plot)#, INLINE)
     
-    return render_template('graph.html', script=script, div=div)
+    return render_template('graph.html', script=script, div=div, title = graph_title)
     #return redirect('http://google.com')
 
 # ticker = 'GOOG'
