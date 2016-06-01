43a42c115258df2b2b8564ad672b467d58463737
Haotian_Daniel_Liu <liuaqiang@gmail.com>
2016-06-01 15:05:29 -0400

    going back

--- app.py
+++ app.py
@@ -36,16 +36,16 @@ def graph():
     page_title = "Generated Graph for " + ticker
     
     # obtaining data through API
-    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/' + ticker + '.json'
-    session = requests.Session()
-    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-    raw_data = session.get(api_url)
-    json_data = raw_data.json()
+    #api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/' + ticker + '.json'
+    #session = requests.Session()
+    #session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+    #raw_data = session.get(api_url)
+    #json_data = raw_data.json()
     # json_data.keys() # display keys of concern
     # [value for key,value in json_data.items() if key not in ['data']]
-    plot_data = { key:value for key, value in json_data.items() if key in ['column_names', 'data'] }
-    plot_data = pd.DataFrame(plot_data['data'],columns = plot_data['column_names'])
-    plot_data['Date'] = pd.to_datetime(plot_data['Date'])
+    #plot_data = { key:value for key, value in json_data.items() if key in ['column_names', 'data'] }
+    #plot_data = pd.DataFrame(plot_data['data'],columns = plot_data['column_names'])
+    #plot_data['Date'] = pd.to_datetime(plot_data['Date'])
     
     #plot = figure(title = "~ Data from Quandle WIKI set ~", x_axis_label='date', x_axis_type='datetime')
     #plot.line(plot_data['Date'], plot_data['Open'], line_width=2, legend = "Opening Price")
